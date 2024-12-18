c93f1c163bd7bab18aac47a2913c25bb53ee71ad
Gui Vieira <guilherme_vieira@me.com>
2024-12-18 17:45:54 -0300

    Change LLM

--- src/hitl/crew.py
+++ src/hitl/crew.py
@@ -16,7 +16,12 @@ class Hitl:
 
     agents_config = "config/agents.yaml"
     tasks_config = "config/tasks.yaml"
-    llm = LLM(model="bedrock/anthropic.claude-3-sonnet-20240229-v1:0")
+    llm = LLM(
+        model="bedrock/anthropic.claude-3-sonnet-20240229-v1:0",
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        aws_region_name=os.getenv("AWS_DEFAULT_REGION"),
+    )
 
     @agent
     def inventor(self) -> Agent:
